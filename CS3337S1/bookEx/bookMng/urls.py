1934d8f9b71a3f8f5f78cb3f5c97f14d10eee94c
J3mm2 <pozjenny68@gmail.com>
2025-04-29 18:31:55 -0700

    Unauthorized users are redirected to login page.

--- CS3337S1/bookEx/bookMng/urls.py
+++ CS3337S1/bookEx/bookMng/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('book_delete/<int:book_id>/', views.book_delete, name='book_delete'),
     path('about_us/', views.about_us, name='about_us'),
     path('toggle_favorite/<int:book_id>/', views.toggle_favorite, name='toggle_favorite'),  # Corrected URL pattern
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
